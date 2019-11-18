17770df2b91bcccd11e3a5d7350222abdc219f57
simletonsl <simpletondl@yandex.ru>
2019-11-18 15:32:25 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="cfpq_redis-simpletondl",
+    version="0.0.1",
+    author="Khoroshev and Terekhov",
+    author_email="simpletondl@yandex.ru",
+    description="Cfpq with python on RedisGraph",
+    long_description="Cfpq with python on RedisGraph",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6'
+)
